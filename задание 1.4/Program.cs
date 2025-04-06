2ccc4aa189345af52b927ef351b1382ff281d17c
kshnchk <kruglovaksusa31@gmail.com>
2025-04-06 17:35:32 +0400

    Add files via upload

---
+++ задание 1.4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+1.4.Создать программу для перевода целого числа N из
+десятичной в пятеричную систему счисления (CC).
+Используйте при разработки программы пояснения к заданию 1.3. 
+*/
+
+namespace задание_1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int N = int.Parse(Console.ReadLine());
+            string b5 = "";
+
+            while (N > 0)
+            {
+                int r = N % 5;
+                b5 = r.ToString() + b5;
+                N /= 5;
+            }
+
+            Console.WriteLine($"Число в пятеричной системе: {b5}");
+
+        }
+    }
+}
---
+++ задание 1.6/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+1.6.Создать программу для перевода числа N7 в десятичную
+систему счисления.
+Используйте при разработки программы пояснения к заданию 1.5.
+*/
+
+namespace задание_1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число в системе счисления с основанием 7:");
+            int sepNum = int.Parse(Console.ReadLine());
+
+            int dNum = 0;
+            int i = 0;
+
+            while (sepNum > 0)
+            {
+                int d = sepNum % 10;
+                dNum += d * (int)Math.Pow(7, i);
+                sepNum /= 10;
+                i++;
+            }
+
+            Console.WriteLine($"Десятичное число: {dNum}");
+        }
+    }
+}
