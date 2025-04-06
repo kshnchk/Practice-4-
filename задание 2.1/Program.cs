f384446294287ca740ec435ab449b908d1423b3b
kshnchk <kruglovaksusa31@gmail.com>
2025-04-06 17:38:40 +0400

    Add files via upload

---
+++ задание 2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+2.1.Создать программу, которая запрашивает у пользователя
+числа до тех пор, пока он не введет 0 Вычислите и выведите
+сумму всех введенных чисел.
+*/
+
+namespace задание_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+
+            do
+            {
+                Console.WriteLine("Введите число (0 для выхода):");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+            } while (num != 0);
+
+            Console.WriteLine($"Сумма всех введенных чисел: {sum}");
+        }
+    }
+}
---
+++ задание 2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+2.2.Дополните код предыдущей задачи для создания программы
+рассчитывающей среднее арифметическое последовательности.
+*/
+
+namespace задание_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+            int c = 0;
+
+            do
+            {
+                Console.WriteLine("Введите число (0 для выхода):");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                c++;
+            } while (num != 0);
+
+            double average = (double)(sum - 0) / (c - 1);
+            Console.WriteLine($"Сумма всех введенных чисел: {sum}");
+            Console.WriteLine($"Среднее арифметическое: {average}");
+
+        }
+    }
+}
